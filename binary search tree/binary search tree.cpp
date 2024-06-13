751131e7da3584a6777f00431908d649650f5018
mdeandwibekti <mdeandwibekti@gmail.com>
2024-06-13 19:24:37 +0700

    case

--- binary search tree/binary search tree.cpp
+++ binary search tree/binary search tree.cpp
@@ -126,6 +126,18 @@ int main() {
 		cin >> ch;
 		cout << endl;
 
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
+
+
 
 
 
