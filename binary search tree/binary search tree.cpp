109cf130ddd7e90a95e1a02986ba1bd852757ffb
mdeandwibekti <mdeandwibekti@gmail.com>
2024-06-13 19:18:35 +0700

    nambahkan void preorder

--- binary search tree/binary search tree.cpp
+++ binary search tree/binary search tree.cpp
@@ -83,6 +83,20 @@ public:
 				inorder(ptr->rightchild);
 			}
 		}
+		void preorder(Node* ptr)
+		{
+			if (ROOT == NULL)
+			{
+				cout << "Tree is empety" << endl;
+				return;
+			}
+			if (ptr != NULL)
+			{
+				cout << ptr->info << " ";
+				preorder(ptr->leftchild);
+				preorder(ptr->rightchild);
+			}
+		}
 		
 
 
